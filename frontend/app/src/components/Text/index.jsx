43b43a3da852fb85e72992e45180c0c0d69dc637
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2022-09-18 01:39:00 -0400

    basic setup complete FE

---
+++ frontend/app/src/components/Text/index.jsx
@@ -0,0 +1,24 @@
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
+import confetti from "../../assets/confetti.gif";
+import "./Text.css";
+
+function Button() {
+  return (
+    <AwesomeButton type="secondary" className="generate-button">
+      <img src={confetti} />
+      Generate Video
+      <img src={confetti} />
+    </AwesomeButton>
+  );
+}
+
+export function TextArea() {
+  return (
+    <div className="container" style={{ cursor: `url(assets/ew.png), auto` }}>
+      <h1>enter your phrase here</h1>
+      <textarea></textarea>
+      <Button />
+    </div>
+  );
+}
